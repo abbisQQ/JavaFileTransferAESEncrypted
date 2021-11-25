c5a86ea4a0a0ddaadd3a52967ec95f04ed6c4e49
Ch. Theodorou <theodoroucharalabos@gmail.com>
2021-11-25 22:57:38 +0200

    Create AESManual.java

---
+++ server/AESManual.java
@@ -0,0 +1,43 @@
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+
+public class AESManual {
+
+
+    public static IvParameterSpec generateIv() {
+        byte[] iv = {-87, 15, -59, 61, -93, -37, -20, -75, 111, 109, 101, 41, 66, 12, 26, 50};
+        //new SecureRandom().nextBytes(iv);
+        return new IvParameterSpec(iv);
+    }
+
+    public static SecretKey getKeyFromPassword(String password, String salt)
+            throws NoSuchAlgorithmException, InvalidKeySpecException {
+
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt.getBytes(), 65536, 256);
+        return new SecretKeySpec(factory.generateSecret(spec)
+                .getEncoded(), "AES");
+    }
+
+
+    public static byte[] decrypt(String algorithm, byte[] cipherText, SecretKey key, IvParameterSpec iv) throws NoSuchPaddingException, NoSuchAlgorithmException,
+            InvalidAlgorithmParameterException, InvalidKeyException,
+            BadPaddingException, IllegalBlockSizeException {
+
+        Cipher cipher = Cipher.getInstance(algorithm);
+        cipher.init(Cipher.DECRYPT_MODE,key,iv);
+        return cipher.doFinal(cipherText);
+
+    }
+
+
+
+
+}
